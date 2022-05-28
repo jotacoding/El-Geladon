e44b8c49473d9d80dd228f62639b7ef607006b65
jotacoding <jvc881@gmail.com>
2022-05-28 00:02:24 -0300

    Primeiro Commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import PaletaLista from './PaletaLista';
+import './Home.css';
+
+
+function Home() {
+    return <div className="Home">
+        <div className="Home__container">
+            <PaletaLista />
+        </div>
+    </div>;
+  }
+  
+  export default Home;
---
+++ src/mocks/paletas.js
@@ -0,0 +1,184 @@
+export const paletas = [
+    
+    {
+        titulo: "Açaí com Leite Condensado",
+        descricao:
+          "Paleta de Açaí recheada de Leite Condesado.",
+        foto: require("../assets/images/acai-com-leite-condensado.png"),
+        preco: 10.0,
+        sabor: "Açaí",
+        recheio: "Leite Condensado",
+        possuiRecheio: true,
+      },
+      {
+        titulo: "Banana com Nutella",
+        descricao:
+          "Paleta de Banana recheada com Nutella.",
+        foto: require("../assets/images/banana-com-nutella.png"),
+        preco: 10.0,
+        sabor: "Banana",
+        recheio: "Nutella",
+        possuiRecheio: true,
+      },
+      {
+        titulo: "Chocolate Belga",
+        descricao:
+          "Paleta de Chocolate Belga.",
+        foto: require("../assets/images/chocolate-belga.png"),
+        preco: 7.0,
+        sabor: "Chocolate Belga",
+        possuiRecheio: false,
+      },
+      {
+        titulo: "Chocolate Belga com Brigadeiro",
+        descricao:
+          "Paleta de Chocolate Belga rechada com delicioso Brigadeiro.",
+        foto: require("../assets/images/chocolate-belga-com-brigadeiro.png"),
+        preco: 10.0,
+        sabor: "Chocolate Belga",
+        recheio: "Brigadeiro",
+        possuiRecheio: true,
+      },
+      {
+        titulo: "Chocolate Branco",
+        descricao:
+          "Paleta de Chocolate Branco.",
+        foto: require("../assets/images/chocolate-branco.png"),
+        preco: 7.0,
+        sabor: "Chocolate Branco",
+        possuiRecheio: false,
+      },
+      {
+        titulo: "Coco",
+        descricao:
+          "Paleta de Coco.",
+        foto: require("../assets/images/coco.png"),
+        preco: 7.0,
+        sabor: "Coco",
+        possuiRecheio: false,
+      },
+      {
+        titulo: "Coco com Doce de Leite",
+        descricao:
+          "Paleta de Coco recheada com Doce de Leite.",
+        foto: require("../assets/images/coco-com-doce-de-leite.png"),
+        preco: 10.0,
+        sabor: "Coco",
+        recheio: "Doce de Leite",
+        possuiRecheio: true,
+      },
+      {
+        titulo: "Cookies",
+        descricao:
+          "Paleta de Cookies.",
+        foto: require("../assets/images/cookies.png"),
+        preco: 7.0,
+        sabor: "Cookies",
+        possuiRecheio: false,
+      },
+      {
+        titulo: "Doce de Leite com Doce de Leite",
+        descricao:
+          "Paleta de Doce de Leite recheada com mais Doce de Leite",
+        foto: require("../assets/images/doce-de-leite-com-doce-de-leite.png"),
+        preco: 10.0,
+        sabor: "Doce de Leite",
+        recheio: "Doce de Leite",
+        possuiRecheio: true,
+      },
+      {
+        titulo: "Limão",
+        descricao:
+          "Paleta de Limão.",
+        foto: require("../assets/images/limao.png"),
+        preco: 7.0,
+        sabor: "Limão",
+        possuiRecheio: false,
+      },
+      {
+        titulo: "Maracujá",
+        descricao:
+          "Paleta de Maracujá.",
+        foto: require("../assets/images/maracuja.png"),
+        preco: 7.0,
+        sabor: "Maracujá",
+        possuiRecheio: false,
+      },
+      {
+        titulo: "Maracujá com Leite Condensado",
+        descricao:
+          "Paleta de Maracujá recheada com Leite Condesado.",
+        foto: require("../assets/images/maracuja-com-leite-condensado.png"),
+        preco: 10.0,
+        sabor: "Maracujá",
+        recheio: "Leite Condensado",
+        possuiRecheio: true,
+      },
+      {
+        titulo: "Milho Verde",
+        descricao:
+          "Paleta de Milho Verde.",
+        foto: require("../assets/images/milho-verde.png"),
+        preco: 7.0,
+        sabor: "Milho Verde",
+        possuiRecheio: false,
+      },
+      {
+        titulo: "Morango",
+        descricao:
+          "Paleta de Morango.",
+        foto: require("../assets/images/morango.png"),
+        preco: 7.0,
+        sabor: "Morango",
+        possuiRecheio: false,
+      },
+      {
+        titulo: "Morango com Leite Condensado",
+        descricao:
+          "A favorita Paleta de Morango recheada com Leite Condesando.",
+        foto: require("../assets/images/morango-com-leite-condensado.png"),
+        preco: 10.0,
+        sabor: "Morango",
+        recheio: "Leite Condensado",
+        possuiRecheio: true,
+      },
+      {
+        titulo: "Morango com Nutella",
+        descricao:
+          "Paleta de Morango recheada com Nutella.",
+        foto: require("../assets/images/morango-com-nutella.png"),
+        preco: 10.0,
+        sabor: "Morango",
+        recheio: "Nutella",
+        possuiRecheio: true,
+      },
+      {
+        titulo: "Ninho com Nutella",
+        descricao:
+          "Paleta de Ninho recheada com Nutella.",
+        foto: require("../assets/images/ninho-com-nutella.png"),
+        preco: 10.0,
+        sabor: "Ninho",
+        recheio: "Nutella",
+        possuiRecheio: true,
+      },
+      {
+        titulo: "Ninho com Ovomaltine",
+        descricao:
+          "Paleta de Ninho recheada de Ovomaltine.",
+        foto: require("../assets/images/ninho-com-ovomaltine.png"),
+        preco: 10.0,
+        sabor: "Ninho",
+        recheio: "Ovomaltine",
+        possuiRecheio: true,
+      },
+      {
+        titulo: "Uva",
+        descricao:
+          "Paleta de Uva.",
+        foto: require("../assets/images/uva.png"),
+        preco: 7.0,
+        sabor: "Uva",
+        possuiRecheio: false,
+      },
+];
